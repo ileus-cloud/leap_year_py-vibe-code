5127bc1e41520aa2d0298c582e25a8cc06b3c6c8
ileus-cloud <i.leus.work@gmail.com>
2026-05-10 07:40:24 +0000

    Add leap year function

--- leap.py
+++ leap.py
@@ -1,2 +1,6 @@
 def is_leap_year(year):
     return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+print("2000:", is_leap_year(2000))
+print("1900:", is_leap_year(1900))
+print("2024:", is_leap_year(2024))
+print("2023:", is_leap_year(2023))
